1efaead7835ede76b43feeff2043ccd692c4b000
NightWolf1298 <nightwolf1298@gmail.com>
2021-01-10 16:18:20 -0500

    Revert Appraise loc, retry for PokeGenie failures

--- run.rb
+++ run.rb
@@ -8,7 +8,7 @@ SSLoc = Struct.new(:loc, :color)
 # 3) Start Poke Genie
 # 4) Open the Pokemon list, and do whatever sorting/filtering you want to select the Pokemon you want to rename
 # 5) Set the desired number of Pokemon to rename:
-POKEMON_TO_PROCESS = 1
+POKEMON_TO_PROCESS = 500
 # 6) Tap the Pokemon you want to start at, so its info is showing
 # 7) RUN
 # 8) DO NOT TOUCH YOUR PHONE WHILE RUNNING - tapping is done by coordinates regardless of what's on screen, so manually
@@ -32,7 +32,9 @@ PHONE_TOP_MARGIN = 0
 # UI element locations (in pixels)
 POKEMON_INFO_CLOSE_LOC = Loc.new(730, 2800)
 POKEMON_INFO_MENU_LOC = Loc.new(1250, 2800)
-POKEMON_INFO_APPRAISE_LOC = Loc.new(1250, 2075) # they moved it up to add Tag
+POKEMON_INFO_APPRAISE_LOC = Loc.new(1250, 2260)
+POKE_GENIE_EDIT_LOC = Loc.new(10, 1010)
+POKE_GENIE_EDIT_DISMISS_LOC = Loc.new(90, 175)
 POKEMON_INFO_APPRAISE_DISMISS_LOC = Loc.new(1030, 1850)
 POKEMON_INFO_RENAME_LOC = Loc.new(720, 1210)
 POKEMON_INFO_RENAME_DISMISS_LOC = Loc.new(720, 1120)
@@ -53,7 +55,15 @@ SCREENSHOT_POKEMON_MENU = SSLoc.new(Loc.new(100, 200), '#4ab483ff') # greenish g
 SCREENSHOT_POKEMON_APPRAISE_INITIAL = SSLoc.new(Loc.new(100, 2600), '#ffffffff') # white background of textbox
 SCREENSHOT_POKEMON_APPRAISE_BARS = SSLoc.new(Loc.new(660, 2130), '#ffffffff') # white background of appraisal bars
 SCREENSHOT_POKEGENIE = SSLoc.new(Loc.new(10, 1010), '#eceff1ff') # gray background of Poke Genie IVision results
+SCREENSHOT_POKEGENIE_RETRY = SSLoc.new(Loc.new(10, 1010), '#f1f3f5ff') # white background of Poke Genie IVision "unable to detect" error
 SCREENSHOT_RENAME = SSLoc.new(Loc.new(1150, 1620), '#43d0a5ff') # greenish background of OK button
+
+# Screenshot return values
+RET_SCREENSHOT_OK = 0
+RET_SCREENSHOT_FALLBACK = 1
+
+# Miscellaneous settings
+SCREENSHOT_FALLBACK_RETRIES = 5 # number of times we can retry for the "unable to detect" Poke Genie error
 
 # Keycode settings - see https://developer.android.com/reference/android/view/KeyEvent#constants_1 for a list
 KEYCODE_BACKSPACE = 67
@@ -80,9 +90,9 @@ def scale(loc, top)
   return Loc.new((PHONE_SIZE.x.to_f / REF_SIZE.x.to_f * loc.x.to_f).to_i, (PHONE_SIZE.y.to_f / REF_SIZE.y.to_f * loc.y.to_f).to_i + (top ? PHONE_TOP_MARGIN : 0))
 end
 
-def adb_verify_screenshot(ssloc, top = false)
+def adb_verify_screenshot(what, ssloc, fallback = nil, top = false)
   unless ADB_SCREENSHOT_VERIFY_ENABLE
-    return
+    return RET_SCREENSHOT_OK
   end
 
   for attempt in 1..ADB_SCREENSHOT_VERIFY_RETRY_ATTEMPTS do
@@ -99,19 +109,26 @@ def adb_verify_screenshot(ssloc, top = false)
 
     File.delete(pngname)
     if pixel == exp
-      return
+      return RET_SCREENSHOT_OK
+    end
+
+    if (fallback != nil)
+      exp_fallback = ChunkyPNG::Color.from_hex(fallback.color)
+      if pixel == exp_fallback
+        return RET_SCREENSHOT_FALLBACK
+      end
     end
 
     delay = attempt <= 2 ? ADB_SCREENSHOT_VERIFY_RETRY_DELAY / 2 : ADB_SCREENSHOT_VERIFY_RETRY_DELAY
-    puts ">> Screenshot validation failed - was #{hex} but expected #{ssloc.color}, waiting #{delay} sec, attempt #{attempt} of #{ADB_SCREENSHOT_VERIFY_RETRY_ATTEMPTS}"
+    puts ">> Screenshot validation failed - '#{what}' was #{hex} but expected #{ssloc.color}, waiting #{delay} sec, attempt #{attempt} of #{ADB_SCREENSHOT_VERIFY_RETRY_ATTEMPTS}"
     sleep(delay)
   end
   throw "Screenshot validation failure exceeded retry attempts. Maybe the game locked up or the servers went down? Start again manually."
 end
 
-def adb_tap(loc, wait, top = false)
+def adb_tap(what, loc, wait, top = false)
   loc = scale(loc, top)
-  puts("adb_tap #{loc.x} #{loc.y}")
+  puts("adb_tap '#{what}' #{loc.x} #{loc.y}")
   if ADB_SEND_ENABLE
     `adb shell input tap #{loc.x} #{loc.y}`
   end
@@ -121,10 +138,10 @@ def adb_tap(loc, wait, top = false)
   sleep(ADB_SEND_ENABLE ? wait : 0)
 end
 
-def adb_swipe(start, finish, duration, wait, top = false)
+def adb_swipe(what, start, finish, duration, wait, top = false)
   start = scale(start, top)
   finish = scale(finish, top)
-  puts "adb_swipe #{start.x} #{start.y} #{finish.x} #{finish.y} #{duration}"
+  puts "adb_swipe '#{what}' #{start.x} #{start.y} #{finish.x} #{finish.y} #{duration}"
   if ADB_SEND_ENABLE
     `adb shell input swipe #{start.x} #{start.y} #{finish.x} #{finish.y} #{duration}`
   end
@@ -134,10 +151,10 @@ def adb_swipe(start, finish, duration, wait, top = false)
   sleep(ADB_SEND_ENABLE ? wait : 0)
 end
 
-def adb_backspace_swipe(loc, dx, hold_duration, swipe_duration, wait, top = false)
+def adb_backspace_swipe(what, loc, dx, hold_duration, swipe_duration, wait, top = false)
   loc = scale(loc, top)
   dx = scale(Loc.new(dx, 0), top).x # scale just the X coordinate
-  puts "adb_backspace_swipe #{loc.x} #{loc.y}"
+  puts "adb_backspace_swipe '#{what}' #{loc.x} #{loc.y}"
   if ADB_SEND_ENABLE
     `adb shell "input swipe #{loc.x} #{loc.y} #{loc.x} #{loc.y} #{hold_duration} && input swipe #{loc.x} #{loc.y} #{loc.x + dx} #{loc.y} #{swipe_duration}"`
   end
@@ -147,8 +164,8 @@ def adb_backspace_swipe(loc, dx, hold_duration, swipe_duration, wait, top = fals
   sleep(ADB_SEND_ENABLE ? wait : 0)
 end
 
-def adb_key(keycode, wait)
-  puts "adb_key #{keycode}"
+def adb_key(what, keycode, wait)
+  puts "adb_key '#{what}' #{keycode}"
   if ADB_SEND_ENABLE
     `adb shell input keyevent #{keycode}`
   end
@@ -158,8 +175,8 @@ def adb_key(keycode, wait)
   sleep(ADB_SEND_ENABLE ? wait : 0)
 end
 
-def adb_key_repeat(keycode, repeat, wait)
-  puts "adb_key_repeat #{keycode} x#{repeat}"
+def adb_key_repeat(what, keycode, repeat, wait)
+  puts "adb_key_repeat '#{what}' #{keycode} x#{repeat}"
   if repeat < 2 # no repeat
     adb_key(keycode, wait)
     return
@@ -180,40 +197,52 @@ def adb_key_repeat(keycode, repeat, wait)
 end
 
 row_even = true
-adb_verify_screenshot(SCREENSHOT_POKEMON_INFO) # make sure we start out in Pokemon info
+adb_verify_screenshot('Pokemon Info Screen', SCREENSHOT_POKEMON_INFO) # make sure we start out in Pokemon info
 for i in 1..POKEMON_TO_PROCESS do
   puts "Scanning #{i} of #{POKEMON_TO_PROCESS}"
 
   # Open appraisal screen and get to the part that shows the bars
-  adb_tap(POKEMON_INFO_MENU_LOC, DELAY_NO_VERIFY)
-  adb_tap(POKEMON_INFO_APPRAISE_LOC, DELAY_NO_VERIFY)
-  adb_tap(POKEMON_INFO_APPRAISE_DISMISS_LOC, DELAY_IVISION)
+  adb_tap('Pokemon Info Menu', POKEMON_INFO_MENU_LOC, DELAY_NO_VERIFY)
+  adb_tap('Appraisal', POKEMON_INFO_APPRAISE_LOC, DELAY_NO_VERIFY)
+  adb_tap('Appraisal Next', POKEMON_INFO_APPRAISE_DISMISS_LOC, DELAY_IVISION)
 
   # Wait for Poke Genie IVision to display
-  adb_verify_screenshot(SCREENSHOT_POKEGENIE)
+  for attempt in 1..SCREENSHOT_FALLBACK_RETRIES do
+    ret = adb_verify_screenshot('Poke Genie IVision Results', SCREENSHOT_POKEGENIE, SCREENSHOT_POKEGENIE_RETRY)
+    if ret == RET_SCREENSHOT_OK
+      # worked this time
+      break
+    end
+    if ret == RET_SCREENSHOT_FALLBACK
+      # PokeGenie says "retry" so dismiss the screen and wait again
+      puts ">> Poke Genie appraisal failed, retrying - attempt #{attempt} of #{SCREENSHOT_FALLBACK_RETRIES}"
+      adb_tap('Poke Genie Edit', POKE_GENIE_EDIT_LOC, DELAY_NO_VERIFY)
+      adb_tap('Poke Genie Edit Dismiss', POKE_GENIE_EDIT_DISMISS_LOC, DELAY_NO_VERIFY)
+    end
+  end
 
   # Dismiss the appraisal screen
-  adb_tap(POKEMON_INFO_APPRAISE_DISMISS_LOC, DELAY_NO_VERIFY)
+  adb_tap('Dismiss Appraisal', POKEMON_INFO_APPRAISE_DISMISS_LOC, DELAY_NO_VERIFY)
 
   # Open rename dialog
-  adb_tap(POKEMON_INFO_RENAME_LOC, DELAY_NO_VERIFY, true) # this button is offset on some phones due to the dead zone at the top
+  adb_tap('Rename', POKEMON_INFO_RENAME_LOC, DELAY_NO_VERIFY, true) # this button is offset on some phones due to the dead zone at the top
 
   # Remove old name - switch to adb_key_repeat if adb_backspace_swipe is unreliable for you
   # adb_key_repeat(KEYCODE_BACKSPACE, POKEMON_NAME_LENGTH, DELAY_NONE)
-  adb_backspace_swipe(GBOARD_BACKSPACE, -200, DELETE_HOLD_DURATION, DELETE_SWIPE_DURATION, DELAY_NONE)
+  adb_backspace_swipe('Remove Old Name', GBOARD_BACKSPACE, -200, DELETE_HOLD_DURATION, DELETE_SWIPE_DURATION, DELAY_NONE)
 
   # Paste new name
-  adb_key(KEYCODE_PASTE, DELAY_NONE)
+  adb_key('Paste New Name', KEYCODE_PASTE, DELAY_NONE)
 
   # Dismiss keyboard
-  adb_tap(POKEMON_INFO_RENAME_DISMISS_LOC, DELAY_NO_VERIFY)
+  adb_tap('Dismiss Keyboard', POKEMON_INFO_RENAME_DISMISS_LOC, DELAY_NO_VERIFY)
 
   # Rename Pokemon
-  adb_tap(POKEMON_INFO_RENAME_OK_LOC, DELAY_VERIFY)
-  adb_verify_screenshot(SCREENSHOT_POKEMON_INFO)
+  adb_tap('Rename OK', POKEMON_INFO_RENAME_OK_LOC, DELAY_VERIFY)
+  adb_verify_screenshot('Pokemon Info Screen', SCREENSHOT_POKEMON_INFO)
 
   # Swipe to next
-  adb_swipe(POKEMON_INFO_SWIPE_START, POKEMON_INFO_SWIPE_END, POKEMON_INFO_SWIPE_DURATION, DELAY_NEXT_POKEMON)
+  adb_swipe('Swipe To Next', POKEMON_INFO_SWIPE_START, POKEMON_INFO_SWIPE_END, POKEMON_INFO_SWIPE_DURATION, DELAY_NEXT_POKEMON)
 
   # Look like a human
   sleep(rand())
